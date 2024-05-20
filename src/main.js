88e4516c82cf1e318462dde6d64ac5ac9297dd40
deruuz <deruuz215@gmail.com>
2024-05-21 01:23:53 +0700

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./Home.vue";
+import Course from "./Course.vue";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/Home", // Tambahkan rute ini
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/Course", // Tambahkan rute ini
+      name: "Course",
+      component: Course,
+    },
+
+    // tambahkan rute lainnya di sini
+  ],
+});
+
+createApp(App).use(router).mount("#app");
