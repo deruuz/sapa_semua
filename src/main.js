b228fb0a566cd554e3f773ef82f48cd8003f4711
deruuz <deruuz215@gmail.com>
2024-06-18 11:01:40 +0700

    apdet

--- src/main.js
+++ src/main.js
@@ -6,13 +6,6 @@ import Home from "./Home.vue";
 import About from "./About.vue";
 import Course from "./Course.vue";
 
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { faUser } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-// library.add(faUser);
-// Vue.component("font-awesome-icon", FontAwesomeIcon);
-
 const router = createRouter({
   history: createWebHistory(),
   routes: [
